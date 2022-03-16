84a6d4282d73971c0809ae1233931bae23eba63c
Arul <aruljothy.s@fleximc.com>
2022-03-16 08:07:02 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MarkerService } from './marker.service';
+import { PopupService } from './popup.service';
+import { ShapeService } from './shape.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MarkerService, PopupService, ShapeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
